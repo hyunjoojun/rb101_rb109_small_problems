6837e5475a96825c1028f170f67ce40ca2106307
Hyunjoo Jun <hjj102088@gmail.com>
2022-06-01 16:41:25 -0400

    Letter Counter (Part2)

---
+++ easy5/letter_counter2.rb
@@ -0,0 +1,34 @@
+=begin
+input: string of words
+output: hash that shows the number of words of different sizes
+
+rules:
+- Words are separated by a space.
+- Non-alphabetic characters are NOT counted.
+- If the string is empty, return a empty hash.
+
+Algorithm:
+- Set an empty hash.
+- Split the words in the string.
+- Delete any non-alphabetic characters.
+- Iterate through the words and set the word's length equal to the hash's key.
+- At the same time, set the value to 1.
+- If the same key exists, add 1 to the existing value.
+- Return the hash.
+=end
+
+def word_sizes(words)
+  counts = Hash.new { |h, k| h[k] = 0 }
+
+  words.split.each do |word|
+    clean_word = word.delete('^A-Za-z')
+    counts[clean_word.size] += 1
+  end
+
+  counts
+end
+
+p word_sizes('Four score and seven.')
+p word_sizes('Hey diddle diddle, the cat and the fiddle!')
+p word_sizes("What's up doc?")
+p word_sizes('')
