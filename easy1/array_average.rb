7f4332ffa2bc7eb019f4f5565112a0abb15699a8
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-26 15:56:54 -0400

    Review array average

--- easy1/array_average.rb
+++ easy1/array_average.rb
@@ -1,9 +1,9 @@
-def average(array)
-  sum = 0
-  array.each { |number| sum += number }
-  sum / array.length
+def average(numbers)
+  sum = numbers.sum
+  average = sum.to_f / numbers.length
+  average.round(2)
 end
 
-puts average([1, 6]) == 3
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 6])
+puts average([1, 5, 87, 45, 8, 8])
+puts average([9, 47, 23, 95, 16, 52])
