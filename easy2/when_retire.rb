4f8564333ae6e3fb34ca3e75aec22485ae437489
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-27 12:27:04 -0400

    Review when will I retire?

--- easy2/when_retire.rb
+++ easy2/when_retire.rb
@@ -1,24 +1,27 @@
 =begin
-input = user's age
-        retirement age
-output = year now
-         year of retirement
-         how many years left until retirement
+Input = current age and retirement age
+Output = Current year, retirement year,
+and how many years left till retirement.
 
-years left == retirement age - age
-year now + years left == year of retirement
-year now = Time.now.year
+Rules
+- Get the input from a user, current age and retirement age.
+- Display when the user will retire and how many years left.
+
+Algorithm:
+- Set two variables current age and retirement age.
+- retirement age - current age = years left
+- current year + years left = retirement year
 =end
 
 puts "What is your age?"
-age = gets.chomp.to_i
+current_age = gets.chomp.to_i
 
 puts "At what age would you like to retire?"
 retirement_age = gets.chomp.to_i
 
-year_now = Time.now.year
-years_left = retirement_age - age
-year_of_retirement = year_now + years_left
+current_year = Time.now.year
+years_left = retirement_age - current_age
+retirement_year = current_year + years_left
 
-puts "It's #{year_now}. You will retire in #{year_of_retirement}."
+puts "It's #{current_year}. You will retire in #{retirement_year}."
 puts "You have only #{years_left} years of work to go!"
