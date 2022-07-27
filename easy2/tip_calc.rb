0b58a19078e2a291a1e24859f99749b2115e97c5
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-27 11:57:12 -0400

    Review tip calc

--- easy2/tip_calc.rb
+++ easy2/tip_calc.rb
@@ -1,18 +1,25 @@
 =begin
-input = the bill amount
-        tip rate (%)
-output = tip amount
-         total amount = (bill + tip)
+Input = Bill amount and a tip rate
+Output = Tip amount and total amount
+
+Rules
+- Ask a user for the bill amount and a tip rate.
+- Display the tip amount and total amount.
+
+Algorithm:
+- Store the user's input into two variables, bill and tip rate.
+- tip amount = bill * (tip percentage / 100)
+- total amount = bill * tip amount
 =end
 
 puts "What is the bill?"
 bill = gets.chomp.to_f
 
 puts "What is the tip percentage?"
-percentage = gets.chomp.to_f
+tip_percentage = gets.chomp.to_f
 
-tip = ((percentage * 0.01) * bill)
-total = (bill + tip)
+tip_amount = (bill * (tip_percentage / 100)).round(2)
+total_amount = (bill + tip_amount).round(2)
 
-puts "The tip is $#{format('%.2f', tip)}"
-puts "The total is $#{format('%.2f', total)}"
+puts "The tip is $#{format("%.2f",tip_amount)}"
+puts "The total is $#{format("%.2f",total_amount)}"
