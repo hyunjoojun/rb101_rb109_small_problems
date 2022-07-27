ce5f676defd39cacd9e97af003c260d8305a891b
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-27 15:40:55 -0400

    Review mutation

--- easy2/mutation.rb
+++ easy2/mutation.rb
@@ -4,5 +4,11 @@ array1.each { |value| array2 << value }
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
 puts array2
 
-# Both arrays will contain the same value.
-# << and .upcase! both mutates the array.
+=begin
+Line 5 outputs Moe, Larry, CURLY, SHEMP, Harpo, CHICO, Groucho, and Zeppo.
+These elements are the same as array1 because on line 3, each method pushes references
+of elements from array1 to array2. Both arrays contain the same string objects
+so when we mutate the strings by upcase! method on line 4, all strings get mutated.
+But array1 and array2 are two different arrays, just their elements refer to the
+same strings.
+=end
