3cabd149c5b1b7e3bb5619fcc80d11c6d059b837
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-27 14:24:07 -0400

    Review odd numbers

--- easy2/odd.rb
+++ easy2/odd.rb
@@ -1,3 +1,15 @@
-(1..99).each { |num| puts num if num.odd? }
+=begin
+Output = print odd numbers
 
-1.upto(99) { |num| puts num if num % 2 == 1 }
+Rules
+- 1 to 99 inclusive, print out odd numbers on a separate line.
+
+Algorithm:
+- puts (1..99) only odd numbers
+=end
+
+puts (1..99).select(&:odd?)
+
+1.upto(99) { |num| puts num if num.odd? }
+
+(1..99).each { |num| puts num if num.odd? }
