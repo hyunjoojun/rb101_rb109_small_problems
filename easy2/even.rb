3315fccd3d52c1ae31947cc96d9471b5ca103ea1
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-27 14:25:55 -0400

    Review even numbers

--- easy2/even.rb
+++ easy2/even.rb
@@ -1,7 +1,15 @@
+=begin
+Output = print even numbers
+
+Rules
+- 1 to 99 inclusive, print out even numbers on a separate line.
+
+Algorithm:
+- puts (1..99) only even numbers
+=end
+
 puts (1..99).select(&:even?)
 
-value = 1
-while value <= 99
-  puts value if value.even?
-  value += 1
-end
+1.upto(99) { |num| puts num if num.even? }
+
+(1..99).each { |num| puts num if num % 2 == 0 }
