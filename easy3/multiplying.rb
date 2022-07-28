4949c41fe0a201fbc55d7ff41537464f3cad8bcc
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-28 11:09:42 -0400

    Review multiplying

--- easy3/multiplying.rb
+++ easy3/multiplying.rb
@@ -1,5 +1,18 @@
+=begin
+Input = Two integers
+Output = An integer
+
+Rules:
+- Multiply two input numbers.
+- Return the result.
+
+Algorithm:
+- num1 * num2 = result
+=end
+
 def multiply(num1, num2)
   num1 * num2
 end
 
-p multiply([5, 4], 3)
+p multiply(5, 3) == 15
+p multiply([2, 4], 2)
