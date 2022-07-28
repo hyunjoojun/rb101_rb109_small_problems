5aaf4e99df0a0eb96ae51e30a30c4ca602f94079
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-28 14:58:20 -0400

    Review palindromic numbers

--- easy3/palindromic_numbers.rb
+++ easy3/palindromic_numbers.rb
@@ -1,12 +1,23 @@
-def palindrome?(string)
-  string == string.reverse
+=begin
+Input = Integer
+Output = Boolean
+
+Rules:
+- Return true if the input number is palindrome.
+- Return false if the input number is not palindrome.
+- Palindrome reads the same forward and backward.
+
+Algorithm:
+- We can use .reverse method if we break the number down into digits.
+- Then compare digits to reversed digits.
+- If they are equal, return true otherwise false.
+=end
+
+def palindromic_number?(number)
+  number.digits == number.digits.reverse
 end
 
-def palindromic_number?(num)
-  palindrome?(num.to_s)
-end
-
-p palindromic_number?(34543)
-p palindromic_number?(123210)
-p palindromic_number?(22)
-p palindromic_number?(5)
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
