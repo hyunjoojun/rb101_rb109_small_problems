75374da37e0a1da6bc3be4123c243787b0ea52e5
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-28 12:21:39 -0400

    Review odd lists

--- easy3/odd.rb
+++ easy3/odd.rb
@@ -1,35 +1,40 @@
 =begin
-input = An array
-output = An array contatining every other element of input array.
-         Start from first element: 1st, 3rd, 5th and so on.
+Input = An array
+Output = An array
+
+Rules:
+- Return an array that contains every other element of the input array.
+- 1st, 3rd, 5th, and so on elements should be in the output array.
+- Return an empty array if the input is an empty array.
+
+Algorithm:
+- Iterate through each element of the input array.
+- Select elements that has index with even number, including 0.
 =end
 
-# def oddities(array)
-#   new_array = []
-#   array.each.with_index do |ele, idx|
-#     new_array << ele if idx.even?
-#   end
-#   new_array
-# end
-
-# def oddities(array)
-#   new_array = []
-#   index = 0
-#   while index < array.size
-#     new_array << array[index]
-#     index += 2
-#   end
-#   new_array
-# end
-
 def oddities(array)
-  array.select.with_index do |ele, idx|
-    ele if idx.even?
-  end
+  array.select.with_index { |_, idx| idx.even? }
 end
 
-p oddities([2, 3, 4, 5, 6])
-p oddities([1, 2, 3, 4, 5, 6])
-p oddities(['abc', 'def'])
-p oddities([123])
-p oddities([])
+p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities(['abc', 'def']) == ['abc']
+p oddities([123]) == [123]
+p oddities([]) == []
+
+# further exploration
+def oddities2(array)
+  array.select.with_index { |_, idx| idx.odd? }
+end
+
+def oddities3(array)
+  new_arr = []
+
+  array.each_with_index do |ele, idx|
+    new_arr << ele if idx.odd?
+  end
+  new_arr
+end
+
+p oddities2([2, 3, 4, 5, 6]) == [3, 5]
+p oddities3(['abc', 'def']) == ['def']
