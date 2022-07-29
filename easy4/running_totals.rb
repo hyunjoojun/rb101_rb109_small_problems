b9ca26b69b419dc6fb36e619e7717c6230440e5a
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-29 11:36:09 -0400

    Review running totals

--- easy4/running_totals.rb
+++ easy4/running_totals.rb
@@ -1,30 +1,37 @@
 =begin
-input: array of numbers
-output: array of numbers with each element has running total
+Input = An array
+Output = An array
 
-rules:
-- array can be empty.
-- array may have one number or multiple numbers.
-- input and output arrays have same number of elements.
-- output array has elements that have the running total.
+Rules:
+- Input array contains integers elements.
+- Retuned array and input array have same number of elements.
+- Each element has the running total from the input array.
 
 Algorithm:
-- Set sum equal to 0
-- Iterate through input array and add each numbers to the sum
-- Return new array
+- Set up a sum = 0
+- Iterate through numbers and add to the sum.
+- Return a new array with running total.
 =end
 
-def running_total(array)
-  sum = 0
-  array.map { |num| sum += num }
-end
-
-# def running_total(array)
+# def running_total(numbers)
 #   sum = 0
-#   array.each_with_object([]) do |num, running_total_array|
-#     running_total_array << sum += num
+#   numbers.map { |num| sum += num }
+# end
+
+# def running_total(numbers)
+#   sum = 0
+#   numbers.each_with_object([]) do |num, arr|
+#     arr << sum += num
 #   end
 # end
+
+def running_total(numbers)
+  total = []
+  numbers.inject(0) do |sum, num|
+    total[total.length] = sum + num
+  end
+  total
+end
 
 p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
