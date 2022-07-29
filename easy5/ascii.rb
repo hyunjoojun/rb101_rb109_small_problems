f7a901679bfc7395b38199c61398844c6c81b4c7
Hyunjoo Jun <hjj102088@gmail.com>
2022-07-29 14:22:06 -0400

    Review ascii

--- easy5/ascii.rb
+++ easy5/ascii.rb
@@ -1,32 +1,30 @@
 =begin
-input: string
-output: integer of ASCII string value
+Input: A string
+Output: An integer
 
-rules:
-- String#ord determines the ASCII value of a character
-- ASCII string value is the sum of the ASCII values
-  of every character in the string.
-- Return the sum in integer
+Rules:
+- Get sum of ASCII values of all the characters in the string.
+- Use #String#ord to determine the ASCII value of a character.
+- Empty string should return 0.
 
 Algorithm:
 - Split the string into characters.
-- Iterate through each characters and use ord method
-  to get the ASCII value of each characters.
-- Set a sum variable = 0.
-- Add all the values to the sum.
+- Iterate through each characters, use .ord to get the value.
+- Add up all the values to get the sum.
 - Return the sum.
 =end
 
-def ascii_value(string)
-  sum = 0
-  string.chars.each { |char| sum += char.ord }
-  sum
+def ascii_value(words)
+  values = []
+
+  words.chars.each { |char| values << char.ord }
+  values.sum
 end
+
+char = "a"
+p char.ord.chr == char
 
 p ascii_value('Four score') == 984
 p ascii_value('Launch School') == 1251
 p ascii_value('a') == 97
 p ascii_value('') == 0
-
-char = "a"
-p char.ord.chr == char
