b87a23edbdaa0a4133bc69e3b842f90f9459edfe
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-01 15:08:09 -0400

    Review bannerizer

--- easy5/bannerizer.rb
+++ easy5/bannerizer.rb
@@ -1,34 +1,30 @@
 =begin
-input: string
-output: string in a box
+Input: A string
+Output: Print a string in a box
 
-rules:
-- Use + - | to make a box.
-- When the string is empty:
-  first line : +--+
-  second line: |  |
-  third line:  |  |
-  fourth line: |  |
-  fifth line:  +--+
-- The string goes in the middle of the box.
+Rules:
+- The input string will always fit the terminal window.
+- Create a box and print it with a string in the middle of the box.
 
 Algorithm:
-- Empty spaces == text.size + 2
-- '-' == text.size + 2
-- put text on the third line.
+- First line: print '+' and '-' * length of the text and another '+'
+- Second line: print '|' and ' ' * length of the text and another '|'
+- Third line: print '|' and the text and another '|'
+- Fourth line: Repeat second line.
+- Last line: Repeat the first line.
 =end
 
 def print_in_box(text)
-  horizontal_rule = "+#{'-' * (text.size + 2)}+"
-  empty_line = "|#{' ' * (text.size + 2)}|"
+  horizontal_line = "+#{'-' * (text.length + 2)}+"
+  empty_line = "|#{' ' * (text.length + 2)}|"
 
-  puts horizontal_rule
+  puts horizontal_line
   puts empty_line
   puts "| #{text} |"
   puts empty_line
-  puts horizontal_rule
+  puts horizontal_line
 end
 
 print_in_box('')
-print_in_box('hello')
+print_in_box('To boldly go where no one has gone before.')
 print_in_box('So coooooooooooool!! Love it!!! :)')
