446527d714f230f6e92d4488768ba40550d3f7ec
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-02 16:03:54 -0400

    Review combine two lists

--- easy7/combine_two_lists.rb
+++ easy7/combine_two_lists.rb
@@ -1,37 +1,38 @@
 =begin
-input: two arrays
-output: combined array with elements taken in alternation
+Input: Two arrays
+Output: An array
 
-rules:
-- Input arrays are not empty.
-- Two arrays have the same number of elements.
-- Elements should alternate.
+Rules:
+- Combine the two input arrays.
+- All elements should be taken in alternation.
+- Both arrays are non-empty.
+- Both arrays have the same number of elements.
 
 Algorithm:
-- Set an empty array.
-- Set index starting 0.
-- Take the first array of first element, push it into the empty array.
-- Take the second array of first element, push it into the array.
-- Increment index by 1.
-- Repeat the process until index is equal to array's length.
+- Shift method takes the first element in the array.
+- Set up an empty array.
+- Take first element from the first array push it into the empty array.
+- Take first element from the second array push it into the new array.
+- Take second element from the first array push it into the new array.
+- Take second element from the second array push it into the new array.
+- Repeat this until both arrays are empty.
 - Return the new array.
 =end
 
-def interleave(arr1, arr2)
-  new_arr = []
-  index = 0
-
-  loop do
-    new_arr << arr1[index] << arr2[index]
-    index += 1
-
-    break if index == arr1.length
-  end
-  new_arr
-end
-
 # def interleave(arr1, arr2)
-#   arr1.zip(arr2).flatten
+#   new_arr = []
+
+#   loop do
+#     new_arr << arr1.shift
+#     new_arr << arr2.shift
+#     break if arr2.empty?
+#   end
+
+#   new_arr
 # end
+
+def interleave(arr1, arr2)
+  arr1.zip(arr2).flatten
+end
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
