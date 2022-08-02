0d230627707925e9ab517508b1dd4d5699ef88d6
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-02 15:13:34 -0400

    Review my list include?

--- easy6/my_list_include.rb
+++ easy6/my_list_include.rb
@@ -1,25 +1,21 @@
 =begin
-input: array and a search value
-output: boolean
+Input: An array and an element
+Output: Boolean
 
-rules:
-- Takes two arguments.
-- If search value is included in the array return true.
-- If search value is not in the array return false.
-- Do not use Array#include? method.
-- Empty array may be used.
+Rules:
+- Check if the element exists in the array.
+- The method returns true or false.
+- If the value is in the array returns true, otherwise false.
+- Do not use include? method.
 
 Algorithm:
 - Iterate through the array.
-- Return true if element is equal to the value.
-- Else, return false.
+- Check if the element is equal to the input argument.
+- Return boolean.
 =end
 
 def include?(arr, value)
-  arr.each do |ele|
-    return true if ele == value
-  end
-  false
+  arr.any? { |ele| ele == value }
 end
 
 p include?([1,2,3,4,5], 3) == true
