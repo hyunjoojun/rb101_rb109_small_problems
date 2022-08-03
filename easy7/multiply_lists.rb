78674a96ff4d592ab7900b08b7dc6285274bc8f7
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-03 12:43:03 -0400

    Review multiply lists

--- easy7/multiply_lists.rb
+++ easy7/multiply_lists.rb
@@ -1,31 +1,25 @@
 =begin
-input: Two arrays
-output: One array
+Input: Two arrays
+Output: An array
 
-rules:
-- The arrays have list of numbers.
-- A pair of numbers that have the same index gets multiplied.
-- The product numbers are returned in an array.
-- Both arrays have the same number of elements.
+Rules:
+- Multiply two numbers that have the same index from two arrays.
+- Create a new array with products.
+- Two arrays have the same number of elements.
 
 Algorithm:
-- Set an empty array.
-- Iterate through the arrays to get product.
-- Push the product into empty array.
-- Return new array.
+- Use map method to return a new array.
+- Use index to pair two numbers and make a new array.
 =end
 
 # def multiply_list(arr1, arr2)
-#   products = []
-
-#   arr1.each_with_index do |num, idx|
-#     products << num * arr2[idx]
+#   arr1.map.with_index do |num, idx|
+#     num * arr2[idx]
 #   end
-#   products
 # end
 
 def multiply_list(arr1, arr2)
-  arr1.zip(arr2).map { |arr| arr.inject(:*) }
+  arr1.zip(arr2).map { |num| num.inject(:*) }
 end
 
-p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+p multiply_list([3, 5, 7], [9, 10, 11])
