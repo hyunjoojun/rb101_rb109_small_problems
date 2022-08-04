9c8f80701ca71a4fedb68fe62921dab8c7106103
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-04 15:14:08 -0400

    Review get the middle

--- easy8/get_the_middle.rb
+++ easy8/get_the_middle.rb
@@ -1,26 +1,23 @@
 =begin
-input: string
-output: middle character or characters
+Input: A string
+Output: A string of middle character
 
-rules:
-- Input string is not empty.
-- If input string length is odd, return one middle character.
-- If input string length is even, return two middle characters.
+Rules:
+- The input string is not empty.
+- Return the middle character of the input string.
+- If the string has an odd length, return one character.
+- If the string has an even length, return two characters.
 
 Algorithm:
-- Get the string length.
-- string length / 2 is the middle character if length is odd.
-- If length is even, (length / 2) - 1 character and length / 2 character is the middle.
-- Return the middle characters.
+- Get the middle index.
+- If the string's length is odd, return one character with the middle index.
+- If the string's length is even, return two characters one in the middle
+  and one before that.
 =end
 
-def center_of(string)
-  center_index = string.length / 2
-  if string.length.odd?
-    string[center_index]
-  else
-    string[center_index - 1, 2]
-  end
+def center_of(words)
+  middle = words.length / 2
+  words.length.even? ? words[(middle - 1)..middle] : words[middle]
 end
 
 p center_of('I love ruby') == 'e'
