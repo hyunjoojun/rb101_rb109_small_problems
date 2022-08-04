7c8f0ae17eb6f5aa3026c8df8c15e57ca8460ccf
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-04 10:40:21 -0400

    Review leading substrings

--- easy8/leading_substrings.rb
+++ easy8/leading_substrings.rb
@@ -1,25 +1,25 @@
 =begin
-input: string
-output: array of substrings
+Input: A string
+Output: An array of substrings
 
-rules:
-- Get all substrings that starts at the beginning of the original string.
-- Return array should be arranged in order from shortest to longest.
+Rules:
+- Return an array with leading substrings.
+- All substrings begin with the first letter of the input string.
+- The elements of the array should be sorted from the shortest to the longest.
 
 Algorithm:
-- Split the string into characters.
 - Set up an empty array.
-- Increment index.
-- Push substrings in order into the empty array.
+- The substrings array will contain string's length number of elements.
+- Push substring that start with 0 index. string[0..index]
 - Return the array.
 =end
 
 def leading_substrings(string)
-  substrings = []
-  string.chars.each_index { |index| substrings << string[0..index] }
-  substrings
+  string.length.times.map do |idx|
+    string[0..idx]
+  end
 end
 
-p leading_substrings('abc')
-p leading_substrings('a')
-p leading_substrings('xyzzy')
+p leading_substrings('abc') == ['a', 'ab', 'abc']
+p leading_substrings('a') == ['a']
+p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
