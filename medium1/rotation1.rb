09f61e43108dca393dd401be528afefe138caf5f
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-05 15:38:22 -0400

    Review rotation 1

--- medium1/rotation1.rb
+++ medium1/rotation1.rb
@@ -1,49 +1,46 @@
 =begin
 Input: An array
-Output: A new array
+Output: A rotated array
 
 Rules:
-- Move the first element of input array to the end of the array.
-- The original array should not be modified.
-- Do not use the method Array#rotate or Array#rotate!.
-- The array may have only one element.
+- The original array shoud not be modified.
+- Move the first element to the end of the array.
+- Do not use rotate method.
 
 Algorithm:
-- Copy the original array.
-- Remove the first element of the copied array and set it equal to a variable.
-- Push the first element of the array to the copied array.
-- Return the copide array.
+- Copy the original array..
+- Save the first element to a variable.
+- Delete the first element, and push the first element to the array.
+- Return the new array.
 =end
 
 def rotate_array(array)
-  copied_arr = array.dup
+  new_arr = array.clone
+  first_element = new_arr.shift
+  new_arr << first_element
 
-  first_ele = copied_arr.shift
-  copied_arr << first_ele
-
-  copied_arr
+  new_arr
 end
 
-def rotate_string(str)
-  chars = str.chars
-  rotated_str = rotate_array(chars)
+def rotate_string(string)
+  array = string.chars
+  rotated_str = rotate_array(array)
   rotated_str.join
 end
 
-def rotate_integer(int)
-  digits = int.digits.reverse
-  rotated_digits = rotate_array(digits)
-  rotated_digits.join
+def rotate_integer(integer)
+  array = integer.digits.reverse
+  rotated_digits = rotate_array(array)
+  rotated_digits.join.to_i
 end
 
-p rotate_string("hello")
-
-p rotate_integer(1234)
-
-p rotate_array([7, 3, 5, 2, 9, 1])
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
 p rotate_array(['a']) == ['a']
 
-p x = [1, 2, 3, 4]
+x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
 p x == [1, 2, 3, 4]                 # => true
+
+p rotate_string('hello') == 'elloh'
+p rotate_integer(123) == 231
