87734307be4af0a48b46c51c57c3cccb0eb1ee73
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-05 12:09:16 -0400

    Review grade book

--- easy9/grade_book.rb
+++ easy9/grade_book.rb
@@ -1,38 +1,35 @@
 =begin
-Input: Three integers
-Output: A letter
+Input: Three scores
+Output: A letter grade of average score
 
 Rules:
-- Input integers are always between 0 and 100.
-- Input integers represent scores.
-- Return value is a letter grade that depends on the average of three scores.
-- 'A' is between 90 and 100.
-- 'B' is between 80 and 89.
-- 'C' is between 70 and 79.
-- 'D' is between 60 and 69.
-- 'F' is between 0 and 59.
+- Input numbers are the scores.
+- Return a letter grade of the average of three scores.
+- 90 to 100 is 'A'
+- 80 to 89 is 'B'
+- 70 to 79 is 'C'
+- 60 to 69 is 'D'
+- 0 to 59 is 'F'
+- Score will be always in between 0 and 100.
 
 Algorithm:
-- Calculate the sum of three input numbers.
-- Calculate the average score from the sum by dividing 3.
-- Write a case statement to check
-  which letter grade is appropriate for the average score.
-- Return the letter in string.
+- Add all the scores and divide it by 3 to get the average.
+- Return the letter according to the average score.
 =end
 
-def get_grade(s1, s2, s3)
-  sum = s1 + s2 + s3
-  average = sum / 3
+def get_grade(score1, score2, score3)
+  average = (score1 + score2 + score3) / 3
 
   case average
-  when (90..) then 'A'
-  when 80..89 then 'B'
-  when 70..79 then 'C'
-  when 60..69 then 'D'
-  else             'F'
+  when 101..   then 'A+'
+  when 90..100 then 'A'
+  when 80..89  then 'B'
+  when 70..79  then 'C'
+  when 60..69  then 'D'
+  else              'F'
   end
 end
 
 p get_grade(95, 90, 93) == "A"
 p get_grade(50, 50, 95) == "D"
-p get_grade(101, 100, 104)
+p get_grade(101, 102, 103) == "A+"
