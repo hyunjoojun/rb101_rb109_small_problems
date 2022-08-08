96e3fab22fc262595e8da8f7d58cab3982730146
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-08 14:35:04 -0400

    2nd review

--- easy5/after_midnight1.rb
+++ easy5/after_midnight1.rb
@@ -3,34 +3,35 @@ Input: An integer
 Output: A string
 
 Rules:
-- Input integer represents number of minutes.
+- The input number is minutes.
+- Conver minutes to the 24-hour time format (hh:mm)
 - If the number is positive, the time is after midnight.
 - If the number is negative, the time is before midnight.
-- Return the time of day in 24 hour format (hh:mm).
-- Do not use Date or Time classes.
+- Do not use ruby's Date and Time classes.
+- Disregard Daylight Savings and Standard Time and other complications.
 
 Algorithm:
-- one day is 1440 minutes
-- (minutes % 1440).divmod(60) will give an array with hour and min
-- convert the array to string in 24 hour format
+- MINS_PER_HOUR = 60
+- HOURS_PER_DAY = 24
+- MINS_PER_DAY = 1440
+- hour = minutes % MINS_PER_DAY / MINS_PER_HOUR
+- remaining_minutes =  minutes - hour * MIN_PER_HOUR
 =end
 
-MINUTES_PER_HOUR = 60
+MINS_PER_HOUR = 60
 HOURS_PER_DAY = 24
-MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+MINS_PER_DAY = MINS_PER_HOUR * HOURS_PER_DAY
 
-def normalize_minutes_to_0_through_1439(minutes)
-  while minutes < 0
-    minutes += MINUTES_PER_DAY
-  end
-
-  minutes % MINUTES_PER_DAY
+def normalize_minutes(minutes)
+  minutes % MINS_PER_DAY
 end
 
-def time_of_day(delta_minutes)
-  delta_minutes = normalize_minutes_to_0_through_1439(delta_minutes)
-  hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
-  format('%02d:%02d', hours, minutes)
+def time_of_day(delta_min)
+  delta_min = normalize_minutes(delta_min)
+  hour = delta_min % MINS_PER_DAY / MINS_PER_HOUR
+  min = delta_min - (hour * MINS_PER_HOUR)
+
+  format('%02d:%02d', hour, min)
 end
 
 p time_of_day(0) == "00:00"
