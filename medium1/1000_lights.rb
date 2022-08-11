f20a9b2f5750c3ee5d23717778c84381d22ad03a
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-11 11:39:46 -0400

    2nd review 1000 lights

--- medium1/1000_lights.rb
+++ medium1/1000_lights.rb
@@ -3,39 +3,45 @@ Input: An integer
 Output: An array
 
 Rules:
-- The input is number of lights and light switches.
-- The input number is also number of rounds that we will toggle switches.
-- Round 1 we toggle all the light swiches, all lights are on.
-- Round 2, we toggle multiples of 2.
-- Round 3, we toggle multiples of 3.
-- Round 4, we toggle multiples of 4 and so on.
-- Method should return an array with numbered lights that are on.
+- There is a bank of switches numbered 1 to n.
+- All the lights are off at the starting point.
+- On round 1, toggle all switches so all the lights are on.
+- On round 2, toggle 2, 4, 6 and so on.
+- On round 3, toggle 3, 6, 9 and so on.
+- Repeat the process until we finish n rounds.
+- n will be the argument of the method we create.
+- Return an array contains switch numbers that are on.
 
 Algorithm:
-- Create a hash to tell if the light is on or off.
-  - Number of light will be the key.
-  - On or off will be the value.
-- We repeat rounds for n, input number repetitions.
-- If the state is off, switch to on, if it's on switch to off.
-- Return an array if the value of hash is on.
+- Create an array with n elements that are false which means off, true means on.
+- Repeat rounds for n number of times.
+- Each round toggle switch number that are divisible by round number.
+  - Round 1 : numbers that are divisible by 1 which are 1, 2, 3 and so on.
+  - Round 2 : numbers that are divisible by 2 which are 2, 4, 6 and so on.
+  - Round 3 : numbers that are divisible by 3 which are 3, 6, 9 and so on.
+- When we get an array of booleans, get the light numbers that are on.
+   = values that are true.
+- Return a new array that contains switch numbers that are on.
 =end
 
-def toggle_lights(n)
-  switches = Array.new(n) { false }
-  on_switches = []
+def toggle_lights(number_of_lights)
+  switches = Array.new(number_of_lights) { false }
 
-  1.upto(n) do |iteration|
-    0.upto(switches.length - 1) do |i|
-      if ((i + 1) % iteration).zero?
-        switches[i] = !switches[i]
-      end
+  1.upto(number_of_lights) do |round|
+    0.upto(number_of_lights - 1) do |idx|
+      switches[idx] = !switches[idx] if (idx + 1) % round == 0
     end
   end
 
-  switches.each_with_index do |switch, idx|
-    on_switches << (idx + 1) if switch
+  boolean_to_numbers(switches)
+end
+
+def boolean_to_numbers(array)
+  numbers = []
+  array.each.with_index do |boolean, idx|
+    numbers << idx + 1 if boolean
   end
-  on_switches
+  numbers
 end
 
 p toggle_lights(5)
