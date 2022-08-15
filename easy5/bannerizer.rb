a70e7ac4c825bbf8e99574241a9c6b6f9b26abb3
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-15 12:56:03 -0400

    2nd review bannerizer

--- easy5/bannerizer.rb
+++ easy5/bannerizer.rb
@@ -7,26 +7,45 @@ Rules:
 - The text should be in the middle of the box.
 
 Algorithm:
-- First line will be horizontal line.
-- Second to fourth lines will be vertical lines.
-- The last line will be same as the first line.
-- The text will be on the middle of third line.
+- horizontal = '+' + '-' * length + 2 + '+'
+- empty = '|' + ' ' * length + 2 + '|'
+- length = string.length
+- middle = '|' + text + '|'
 =end
 
 MAX_LENGTH = 76
 
-def print_in_box(text)
-  spaces = text.length > MAX_LENGTH ? MAX_LENGTH : text.length
-  horizontal = '+' + '-' * (spaces + 2) + '+'
-  vertical = '|' + ' ' * (spaces + 2) + '|'
+def split_text(text)
+  multiline_text = []
+  loop do
+    if text.length > MAX_LENGTH
+      multiline_text << "| #{text.slice!(0..MAX_LENGTH - 3)} |"
+    else
+      filler = ' ' * (MAX_LENGTH - text.length - 2)
+      multiline_text << "| #{text.slice!(0..)}#{filler} |"
+    end
+    break if text.length == 0
+  end
 
-  puts horizontal
-  puts vertical
-  puts "| #{text} |"
-  puts vertical
-  puts horizontal
+  multiline_text
 end
 
-print_in_box('hi')
+def print_in_box(text)
+  spacer = text.length > MAX_LENGTH ? MAX_LENGTH : (text.length + 2)
+  horizontal = '+' + '-' * spacer + '+'
+  empty = '|' + ' ' * spacer + '|'
+
+  if text.length < MAX_LENGTH
+    middle = "| #{text} |"
+  elsif text.length > MAX_LENGTH
+    middle = split_text(text)
+  end
+
+  puts [horizontal, empty, middle, empty, horizontal]
+end
+
 print_in_box('To boldly go where no one has gone before.')
 print_in_box('')
+print_in_box('Life is short and we have never too much time for gladdening the' +
+  'hearts of those who are traveling the dark journey with us. Oh be swift to' +
+  'love, make haste to be kind. -Henri Frederic Amiel')
