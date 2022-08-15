5e5e408755da3f2b4d06a0225d518928962677ca
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-15 15:21:53 -0400

    3rd review anagrams

--- easy9/group_anagrams.rb
+++ easy9/group_anagrams.rb
@@ -3,18 +3,17 @@ Input: An array of words
 Output: Print out groups of words into arrays.
 
 Rules:
-- Print out groups of words that are anagrams.
-- Anagrams are words that have same letters but in a different order.
-- Group them into arrays and print out the arrays.
+- Group the words into anagrams.
+- Anagrams are words that have same letters, but in different order.
+- Print out one group of anagrams as an array.
 
 Algorithm:
-- Set up an empty hash.
-- Iterate through the words.
-- Sort the letters in alphabetical order, compare it to other words.
-- Set the sorted letter word as a key.
-- If the key exists in the hash, push the word into the array.
-- If the key does not exist, set up new key.
-- Print out the words using the hash.
+- Iterate through the words array.
+- Split each words into characters.
+- Sort the characters and compare it to other words.
+- If sorted characters in a word equals other word, group them into one array.
+- Iterate through all the words to group all of them.
+- Print out each arrays.
 =end
 
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
@@ -26,7 +25,7 @@ anagrams = {}
 words.each do |word|
   key = word.chars.sort.join
 
-  if anagrams.has_key?(key)
+  if anagrams.key?(key)
     anagrams[key] << word
   else
     anagrams[key] = [word]
