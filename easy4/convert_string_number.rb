9abf08a04ed3ae446a2880470ffbfe0177ea13e2
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-15 11:51:29 -0400

    3rd review convert str to num

--- easy4/convert_string_number.rb
+++ easy4/convert_string_number.rb
@@ -7,22 +7,25 @@ Rules:
 - Do not use to_i method.
 
 Algorithm:
-- Create an array of string integers.
+- Create an array that contains string numbers as elements.
 - Split the string into characters.
 - Iterate through each characters and get the index of created array.
 - Get the array of integers.
 - Conver the array into the integers.
 =end
 
-INTEGERS = %w(0 1 2 3 4 5 6 7 8 9)
+STRING_NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def string_to_integer(string)
   digits = string.chars.map do |num|
-    INTEGERS.index(num)
+    STRING_NUMBERS.index(num)
   end
+  convert_array_to_num(digits)
+end
 
+def convert_array_to_num(arr)
   value = 0
-  digits.each { |digit| value = 10 * value + digit }
+  arr.each { |digit| value = 10 * value + digit }
   value
 end
 
