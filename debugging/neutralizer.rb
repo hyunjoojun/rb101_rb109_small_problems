0bfa1b6c8dc508ce3664020af8145222ef1d1281
Hyunjoo Jun <hjj102088@gmail.com>
2022-08-19 11:22:25 -0400

    Neutralizer

---
+++ debugging/neutralizer.rb
@@ -0,0 +1,18 @@
+def neutralize(sentence)
+  words = sentence.split(' ')
+  words.reject! do |word|
+    negative?(word)
+  end
+
+  words.join(' ')
+end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts neutralize('These dull boring cards are part of a chaotic board game.')
