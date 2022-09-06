c1e539220582b066ab4f44a12ea4b48e7e4910dd
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-06 11:03:03 -0400

    2nd review

--- easy4/short_long.rb
+++ easy4/short_long.rb
@@ -25,3 +25,7 @@ end
 p short_long_short('abc', 'defgh') == "abcdefghabc"
 p short_long_short('abcde', 'fgh') == "fghabcdefgh"
 p short_long_short('', 'xyz') == "xyz"
+
+# def short_long_short(str1, str2)
+#   str1.size > str2.size ? str2 + str1 + str2 : str1 + str2 + str1
+# end
