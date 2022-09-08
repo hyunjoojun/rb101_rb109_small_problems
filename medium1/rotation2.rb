7a44b73a64a08f4de78cfd3976f7c1ccb5d99ace
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-08 11:50:48 -0400

    2nd review rotation 2

--- medium1/rotation2.rb
+++ medium1/rotation2.rb
@@ -3,26 +3,28 @@ Input: Two integers
 Output: One integer
 
 Rules:
-- Rotate the last n digits of the number.
-- The first argument is the number, second argument is n digits.
-- If the second argument is 1, return the same number.
-- Use the rotate_array method from previous exercise.
-- The second argument will always be positive.
+- Rotate the last n digit of the input number.
+- The first argument is the number that needs to be rotated.
+- The second argument is the n digit which is the digit that will be moved.
+- If the second argument is 1, the original number is returned.
+- Use the rotate_array method from the previous exercise.
+- n will be always a positive integer.
 
 Algorithm:
-- The second argument n digits will be taken out and placed in the back of the number.
-- We are moving one chosen number to the end.
-- Set a variable chosen_num.
-- Delete that chosen_num and push it back to the number.
-- Return the rotated number.
+- Make an array of digits.
+- moving_digit = array[-n]
+- Delete the moving digit from the original array.
+- Push the deleted digit to the end of the array.
+- Join the array and convert it to the integer.
 =end
 
-def rotate_rightmost_digits(num, digit)
-  chosen_num = num.to_s[-digit]
-  num_string = num.to_s.delete(chosen_num)
+def rotate_rightmost_digits(number, digit)
+  digits_arr = number.digits.reverse
+  moving_digit = digits_arr[-digit]
 
-  num_string << chosen_num
-  num_string.to_i
+  digits_arr.delete(moving_digit)
+  rotated_arr = digits_arr << moving_digit
+  rotated_arr.join.to_i
 end
 
 p rotate_rightmost_digits(735291, 1) == 735291
@@ -31,3 +33,11 @@ p rotate_rightmost_digits(735291, 3) == 735912
 p rotate_rightmost_digits(735291, 4) == 732915
 p rotate_rightmost_digits(735291, 5) == 752913
 p rotate_rightmost_digits(735291, 6) == 352917
+
+# def rotate_rightmost_digits(num, digit)
+#   chosen_num = num.to_s[-digit]
+#   num_string = num.to_s.delete(chosen_num)
+
+#   num_string << chosen_num
+#   num_string.to_i
+# end
