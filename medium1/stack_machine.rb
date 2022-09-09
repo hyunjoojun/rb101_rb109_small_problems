8af8afc61eb51d0039176459c249f2154d50f947
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-09 11:44:28 -0400

    Review Stack Machine

--- medium1/stack_machine.rb
+++ medium1/stack_machine.rb
@@ -3,40 +3,46 @@ Input: A string of commands
 Output: values (integer)
 
 Rules:
-- Write a method that takes a stack-and-register programming language.
-- Each operation operates on a register.
-- The register begins with 0.
+- Write a method that operates commands that are on the list.
+- All operations operates on a register which is the current value.
 - The register is not part of the stack.
+- All operations are integer opreations.
+- Initialize the register to 0.
 
 Algorithm:
-- The register will be a number.
-- A stack will be an array.
-- Any number in the input string will become a register value.
-- Split the input string into elements.
-- Each elements will do some operation with the register and stack.
+- Set register equal to 0.
+- Set stack as an empty array.
+- Split the input string into operations.
 - Follow the problem description to create cases.
 =end
 
-def minilang(commands)
+def minilang(program)
   register = 0
   stack = []
+  commands = program.split
 
-  commands.split.each do |command|
+  commands.each do |command|
     if command.to_i.to_s == command
       register = command.to_i
     else
-      case command
-      when 'PUSH' then stack << register
-      when 'ADD' then register += stack.pop
-      when 'SUB' then register -= stack.pop
-      when 'MULT' then register *= stack.pop
-      when 'DIV' then register /= stack.pop
-      when 'MOD' then register %= stack.pop
-      when 'POP' then register = stack.pop
-      when 'PRINT' then puts register
-      end
+      register = main_program(command, register, stack)
     end
   end
+end
+
+def main_program(command, register, stack)
+  case command
+  when 'PRINT' then puts register
+  when 'PUSH' then stack << register
+  when 'MULT' then register *= stack.pop
+  when 'ADD' then register += stack.pop
+  when 'POP' then register = stack.pop
+  when 'SUB' then register -= stack.pop
+  when 'DIV' then register /= stack.pop
+  when 'MOD' then register %= stack.pop
+  else puts 'Invalid command'
+  end
+  register
 end
 
 minilang('PRINT')
@@ -48,3 +54,4 @@ minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
 minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
 minilang('-3 PUSH 5 SUB PRINT')
 minilang('6 PUSH')
+minilang('HELLO')
