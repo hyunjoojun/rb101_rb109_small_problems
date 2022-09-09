2d640cf08f0af62fd5b784aaa0a7b2a90bac03e2
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-09 12:17:41 -0400

    Review word to digit

--- medium1/word_to_digit.rb
+++ medium1/word_to_digit.rb
@@ -3,25 +3,33 @@ Input: A string
 Output: A string but words converted to string of digits
 
 Rules:
-- The input string contains alphabetical words.
-- Some words are numbers in words.
-- Convert numbers to integer but in string format.
-- Return the same string but only numbers transformed into integers.
+- Input string contains words.
+- If words represent numbers, convert them into integers.
+- All other words and punctuation remains the same.
 
 Algorithm:
-- Create an array with words 'zero', 'one' and so on.
-- Iterate through the array and substitute the word with its index.
-- Return the string.
+- Create an array of number words.
+- Substitute the word into its index.
+- Return a new string.
 =end
 
-NUMBERS = %w(zero one two three four five six seven eight nine)
+NUMBER_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-def word_to_digit(sentence)
-  NUMBERS.each do |word|
-    sentence.gsub!(/\b#{word}\b/, NUMBERS.index(word).to_s)
+def word_to_digit(string)
+  NUMBER_WORDS.each do |word|
+    string.gsub!(word, NUMBER_WORDS.index(word).to_s)
   end
-  sentence
+  string
 end
+
+# NUMBERS = %w(zero one two three four five six seven eight nine)
+
+# def word_to_digit(sentence)
+#   NUMBERS.each do |word|
+#     sentence.gsub!(/\b#{word}\b/, NUMBERS.index(word).to_s)
+#   end
+#   sentence
+# end
 
 # NUMBERS = {'zero' => "0", 'one' => "1", 'two' => "2", 'three' => "3", 'four' => "4", 'five' => "5", 'six' => "6", 'seven'=> "7", 'eight' => "8", 'nine' => "9"}
 
