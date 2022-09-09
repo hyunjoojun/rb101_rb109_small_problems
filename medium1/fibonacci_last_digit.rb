dab648faac896981f45a926c4eaa71dceb58313a
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-09 15:37:07 -0400

    Review fibonacci last digit

--- medium1/fibonacci_last_digit.rb
+++ medium1/fibonacci_last_digit.rb
@@ -1,33 +1,25 @@
 =begin
 Input: An integer (nth)
-Output: An integer (nth number in fibonacci numbers)
+Output: The last digit of fibonacci number
 
 Rules:
-- Return the last digit of the number that's nth of fibonacci numbers.
-- nth is the input integer of the method.
+- Return the last digit of nth fibonacci number.
+- nth is the input number.
 
 Algorithm:
-- Fibonacci numbers starts with two 1s = [1, 1]
-- Add two numbers until its length = input number
-- Get the last number of the array.
-- Return the last digit = (number % 10)
+- The last digit repeats every 60 numbers.
+- We can get the last digit with % 10.
 =end
 
-# def fibonacci_last(nth)
-#   fibs = [1, 1]
-#   until nth == fibs.length do
-#     fibs << fibs.last(2).sum
-#   end
-#   fibs.last % 10
-# end
+def fibonacci(nth)
+  fibs = [1, 1]
+
+  (nth - 2).times { fibs << fibs.last(2).sum }
+  fibs.last
+end
 
 def fibonacci_last(nth)
-  last_2 = [1, 1]
-  3.upto(nth) do
-    last_2 = [last_2.last, (last_2.first + last_2.last) % 10]
-  end
-
-  last_2.last
+  fibonacci(nth % 60) % 10
 end
 
 p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
@@ -36,3 +28,4 @@ p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 35422484817926
 p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
 p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
 p fibonacci_last(123456789) # -> 4
+p fibonacci_last(123456789987745) # -> 5
