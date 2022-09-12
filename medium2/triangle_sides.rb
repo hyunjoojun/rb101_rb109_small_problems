07742b0598f49fa3c3cbf499c46a5a8558909dfc
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-12 16:18:12 -0400

    Review triangle sides

--- medium2/triangle_sides.rb
+++ medium2/triangle_sides.rb
@@ -3,22 +3,23 @@ Input: The lengths of 3 sides of a triangle (integers)
 Output: A symbol, name of triangle or invalid
 
 Rules:
-- When all 3 sides are equal in length = equilateral
-- When 2 sides are equal, 3rd is different = isosceles
-- When all 3 sides are different in length = scalene
-- A valid triangle has a sum of two shortest sides that is greater than the longest side.
-- All sides must be greater than 0.
-- Return :invalid if these conditions are not satisfied.
-- Else, return the triangle's name in symbol.
+- The sum of the two shortest sides must be greater than the longest side.
+- All the sides must be greater than 0.
+- Equilateral trianges: all 3 sides are equal.
+- Isosceles triangles: 2 sides are equal.
+- Scalene triangles: all 3 sides are different.
+- 3 arguments are the lengths of the triangles.
+- Return a symbol, :equilateral, :isosceles, :scalene, or :invalid.
 
 Algorithm:
-- Create an array of 3 numbers and sort them.
-- If any of them are 0, return :invalid.
-- Add 1st and 2nd number and check if the sum is greater than 3rd number.
-- If it's not greater, return :invalid.
-- Now if all the sides are equal, return :equilateral.
-- If not, check if any two are equal, return :isosceles.
-- If they are all different, return :scalene.
+- Create an array of sides that is sorted by length.
+- [s1, s2, s3] s1 is the shortest side, s3 is the longest side.
+- This has to be true: s1 + s2 > s3
+- All sides have to be greater than 0.
+- If these conditions pass, check each sides.
+- s1 == s2 == n3, :equilateral
+- s1 == s2 || s2 == s3 || s1 == s3, :isosceles
+- s1 != s2, s2 != s3 :scalene
 =end
 
 def triangle(s1, s2, s3)
