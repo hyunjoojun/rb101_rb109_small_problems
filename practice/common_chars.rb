6e3220024e2bf24865894058dc65c7f3e9284a1e
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-16 13:07:19 -0400

    Common characters

---
+++ practice/common_chars.rb
@@ -0,0 +1,37 @@
+=begin
+Given array of strings made only of lowercase letters, return array of
+all characters that show up in all strings within the given array
+(including duplicates) For example, if a character occurs 3 times in
+all strings, but not 4 times, you need to include that character three
+times in the final answer.
+
+Input: An array of strings
+Output: An array of common letters in all the strings
+
+Rules:
+- The input array has strings that are all lowercase letters.
+- If a character appears in all the strings, return it within the array.
+- Include all the duplicates.
+
+Algorithm:
+- We can use the first string to compare all the letters.
+- Split the first string into characters.
+- Iterate through each characters and check if all the strings has the character.
+- If all strings have the character, select the character and return it.
+=end
+
+def common_chars(array)
+  array = array.map { |word| word.dup }
+
+  first_str = array.shift
+  first_str.chars.select do |char|
+    array.all? do |word|
+      word.sub!(char, '')
+    end
+  end
+end
+
+p common_chars(['bella', 'label', 'roller']) == ['e', 'l', 'l']
+p common_chars(['cool', 'lock', 'cook']) == ['c', 'o']
+p common_chars(['hello', 'goodbye', 'booya', 'random']) == ['o']
+p common_chars(%w(aabbaaaa ccdddddd eeffee ggrrrrr yyyzzz)) == []
