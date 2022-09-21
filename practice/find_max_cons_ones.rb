bd287488c9cf17d6a6f923f1d9671e504e588e56
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-21 13:11:42 -0400

    Find max consecutive ones

---
+++ practice/find_max_cons_ones.rb
@@ -0,0 +1,37 @@
+# Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+# Input: An array of numbers
+# Output: An integer, length of consecutive 1s.
+
+# Rules:
+# Return the longest length of consecutive 1s in the array.
+
+# Set up length = 0
+# consecutive_lengths = []
+# Iterate through each number and check if it's 1.
+# If it's 1, Add 1 to the length.
+# If it's not 1 push the length to an empty array and set the length = 0.
+# Repeat the process and get the maximum number in the lengths array.
+
+def find_max_consecutive_ones(array)
+  length = 0
+  consecutive_lengths = []
+  array.each do |num|
+    if num == 1
+      length += 1
+      consecutive_lengths << length
+    else
+      length = 0
+    end
+  end
+  return 0 if consecutive_lengths.empty?
+
+  consecutive_lengths.max
+end
+
+p find_max_consecutive_ones([1,1,0,0,1,0]) == 2
+p find_max_consecutive_ones([0,1,0,1,1,0,1,1,1]) == 3
+p find_max_consecutive_ones([0,0,0]) == 0
+p find_max_consecutive_ones([0,1,0]) == 1
+p find_max_consecutive_ones([1,0,1,1,0,1]) == 2
+p find_max_consecutive_ones([1,1,0,1,1,1]) == 3
