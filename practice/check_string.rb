1562f0087bddac48f8637bb0ed5e43e6ef5419ed
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-22 13:08:47 -0400

    Check string

---
+++ practice/check_string.rb
@@ -0,0 +1,25 @@
+# Given a string s consisting of only the characters 'a' and 'b', return true if every 'a'
+# appears before every 'b' in the string. Otherwise, return false.
+
+# Input: String
+# Output: true or false
+
+# 'a' has to appear before 'b'.
+# 'a' cannot be after 'b'.
+# If there is 'b' before 'a' return false
+# If there is no 'a' return false
+# If there is only 'a' return true
+# If there is 'b' in the string, the string has to end with 'a', false
+
+def check_string(string)
+  return false if !string.include?('a')
+
+  string.chars == string.chars.sort
+end
+
+p check_string("aba") == false
+p check_string("aaa") == true
+p check_string("aabb") == true
+p check_string("abab") == false
+p check_string("aaabbb") == true
+p check_string("bbb") == false
