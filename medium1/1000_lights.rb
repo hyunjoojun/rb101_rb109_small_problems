1d8ca5d7805ac9cbb639143ea1f8efae3e64b845
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-28 13:04:53 -0400

    5th review 1000 lights

--- medium1/1000_lights.rb
+++ medium1/1000_lights.rb
@@ -3,19 +3,19 @@ Input: An integer
 Output: An array
 
 Rules:
-- The input integer represents number of light switches.
-- The switches are numbered 1 to n.
-- n also represents the number of rounds that I need to toggle switches.
-- If n = 5
-  - Round 1 : Every light is on.
-  - Round 2 : Toggle 2, 4 => 1, 3, 5 are on.
-  - Round 3 : Toggle 3 => 1, 5 are on.
-  - Round 4 : Toggle 4 => 1, 4, 5 are on.
-  - Round 5 : Toggle 5 => 1, 4 are on.
-- Return an array where the elements are on switches. [1, 4]
-- If n = 10, return [1, 4, 9]
+- The argument represents number of switches and also number of rounds.
+- For each round, we toggle lights which makes the lights on or off.
+- Example if n = 5.
+- Round 1: toggle all of them so all the lights are on.
+- Round 2: toggle 2, 4 so 1, 3, 5 are on.
+- Round 3: toggle 3, so 1, 5 are on.
+- Round 4: toggle 4 so 1, 4, 5 are on.
+- Round 5: toggle 5 so 1, 4 are on.
+- Return the lights that are on as an array => [1, 4]
 
 Algorithm:
+- A method to keep track of lights are on or off.
+- A method for toggling switches.
 - Create an array with n number of elements.
 - On / Off can be created as True / False.
 - if n = 5, [false, false, false, false, false] meaning lights are off.
@@ -23,37 +23,24 @@ Algorithm:
 - [true, false, false, true, false], return index + 1 of trues as an array.
 =end
 
-def on_lights(number_of_lights)
-  lights = Array.new(number_of_lights)
+def toggle_switches(n)
+  switches = Array.new(n) { false }
 
-  number_of_lights.times do |round|
-    lights.map!.with_index do |boolean, idx|
-      (idx + 1) % (round + 1) == 0 ? !boolean : boolean
+  1.upto(n) do |round|
+    1.upto(switches.length) do |index|
+      switches[index - 1] = !switches[index - 1] if index % round == 0
     end
   end
-  boolean_to_numbers(lights)
+  on_switches(switches)
 end
 
-def boolean_to_numbers(array)
+def on_switches(array)
   numbers = []
-  array.each.with_index do |boolean, idx|
-    numbers << idx + 1 if boolean
+  array.each.with_index do |switch, idx|
+    numbers << idx + 1 if switch
   end
   numbers
 end
 
-p on_lights(5)
-p on_lights(10)
-p on_lights(1000)
-
-# def toggle_lights(number_of_lights)
-#   switches = Array.new(number_of_lights) { false }
-
-#   1.upto(number_of_lights) do |round|
-#     0.upto(number_of_lights - 1) do |idx|
-#       switches[idx] = !switches[idx] if (idx + 1) % round == 0
-#     end
-#   end
-
-#   boolean_to_numbers(switches)
-# end
+p toggle_switches(5)
+p toggle_switches(10)
