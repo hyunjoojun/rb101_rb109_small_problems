1b6a60f9474348f7941211e0a7ea07b60f20d89c
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-28 16:32:20 -0400

    2nd review word to digit

--- medium1/word_to_digit.rb
+++ medium1/word_to_digit.rb
@@ -3,24 +3,25 @@ Input: A string
 Output: A string but words converted to string of digits
 
 Rules:
-- Input string contains words.
-- If words represent numbers, convert them into integers.
-- All other words and punctuation remains the same.
+- Convert any number words to integers in the given string.
+- All other words and punctuation stays the same.
 
 Algorithm:
 - Create an array of number words.
-- Substitute the word into its index.
-- Return a new string.
+- If they are in the string, convert it to its index.
+- Return the new string.
 =end
 
-NUMBER_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+NUMBER_WORDS = %w(zero one two three four five six seven eight nine)
 
 def word_to_digit(string)
-  NUMBER_WORDS.each do |word|
-    string.gsub!(word, NUMBER_WORDS.index(word).to_s)
+  NUMBER_WORDS.each_with_index do |word, idx|
+    string.gsub!(word, idx.to_s)
   end
   string
 end
+
+p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
 
 # NUMBERS = %w(zero one two three four five six seven eight nine)
 
@@ -39,5 +40,3 @@ end
 #   end
 #   str
 # end
-
-p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
