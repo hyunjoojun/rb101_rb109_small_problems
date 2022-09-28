9735ed78621ed206a5a49806a7d45bb6a3b067c4
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-28 12:11:03 -0400

    3rd review rotation2

--- medium1/rotation2.rb
+++ medium1/rotation2.rb
@@ -3,12 +3,13 @@ Input: Two integers
 Output: One integer
 
 Rules:
-- Rotate the last n digit of the input number.
-- The first argument is the number that needs to be rotated.
-- The second argument is the n digit which is the digit that will be moved.
-- If the second argument is 1, the original number is returned.
+- Two arguments: number, a digit.
+- Move the n digit to the end of the number.
+- If n = 2, moving the 2nd to the last digit to the end of the number.
+- 735291, 2 => 735219
+- When n = 1 the same number is returned.
 - Use the rotate_array method from the previous exercise.
-- n will be always a positive integer.
+- n will be always positive integer.
 
 Algorithm:
 - Make an array of digits.
@@ -18,13 +19,18 @@ Algorithm:
 - Join the array and convert it to the integer.
 =end
 
-def rotate_rightmost_digits(number, digit)
-  digits_arr = number.digits.reverse
-  moving_digit = digits_arr[-digit]
+def rotate_rightmost_digits(num, n)
+  result = []
 
-  digits_arr.delete(moving_digit)
-  rotated_arr = digits_arr << moving_digit
-  rotated_arr.join.to_i
+  digits = num.digits.reverse
+  digits.each do |digit|
+    next if digit == digits[-n]
+
+    result << digit
+  end
+
+  result << digits[-n]
+  result.join.to_i
 end
 
 p rotate_rightmost_digits(735291, 1) == 735291
@@ -33,6 +39,15 @@ p rotate_rightmost_digits(735291, 3) == 735912
 p rotate_rightmost_digits(735291, 4) == 732915
 p rotate_rightmost_digits(735291, 5) == 752913
 p rotate_rightmost_digits(735291, 6) == 352917
+
+# def rotate_rightmost_digits(number, digit)
+#   digits_arr = number.digits.reverse
+#   moving_digit = digits_arr[-digit]
+
+#   digits_arr.delete(moving_digit)
+#   rotated_arr = digits_arr << moving_digit
+#   rotated_arr.join.to_i
+# end
 
 # def rotate_rightmost_digits(num, digit)
 #   chosen_num = num.to_s[-digit]
