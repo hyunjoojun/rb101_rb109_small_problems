8494db998078a31b0c65e3b83cdfe82feafcdd19
Hyunjoo Jun <hjj102088@gmail.com>
2022-09-28 16:10:30 -0400

    2nd review diamonds

--- medium1/diamonds.rb
+++ medium1/diamonds.rb
@@ -3,30 +3,30 @@ Input: An integer
 Output: Display a diamond in n x n grid
 
 Rules:
-- The input integer will always be an odd number.
-- Display 4 pointed diamond in n x n grid where n is the input number.
-- Use empty spaces and * to make a diamond.
+- The input integer will always be an odd integer.
+- Display a 4 pointed diamond in n x n grid.
 
 Algorithm:
-- 1st line upto middle line, the number of stars increase.
-- 1st line starts with 1 star.
-- Until the middle line (n / 2), number of stars increase by 2.
-- The middle line the number of stars = n
-- The second half of diamond can be repeated from middle to 1.
+- The middle line gets n number of stars.
+- Displays n number of lines.
+- The first line will always start with 1 star.
+- The number of stars increase by 2 every line until we get to the middle line.
+- After the middle line, number of stars decrease by 2 until we get to 1 star.
 =end
 
-def diamond(length)
-  middle = (length / 2) + 1
+def diamond(number)
+  middle = number / 2 + 1
   number_of_stars = 1
 
   middle.times do
-    puts ('*' * number_of_stars).center(length)
+    puts ('*' * number_of_stars).center(number)
     number_of_stars += 2
   end
 
-  number_of_stars -= 4
-  (length / 2).times do
-    puts ('*' * number_of_stars).center(length)
+  number_of_stars = number - 2
+
+  until number_of_stars < 1
+    puts ('*' * number_of_stars).center(number)
     number_of_stars -= 2
   end
 end
